eebd541823c732b7a6667c257fbe4b52237bba3a
laurensruiz <laurenruiz97@yahoo.com>
2022-03-24 20:21:27 -0400

    setup [GET] from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +11,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+  const [currentCharacter, setCurrentCharacter] = useState([])
+
+  const openDetails = id => {
+    setCurrentCharacter(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacter(null)
+  }
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+      .then(res => {
+        setCharacters(res.data.results)
+        
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  console.log(characters)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
